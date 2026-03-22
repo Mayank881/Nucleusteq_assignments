9136f46d1202b02e74062081890cee787825ca3d
Mayank <mayankkukraja.2006@gmail.com>
2026-03-22 19:17:57 +0530

    feat : add students data

---
+++ Mayank_frontend/js/mayank_student_analyzer.js
@@ -0,0 +1,36 @@
+// add the data structure for the program calculation.
+const students = [
+    {
+        name: "Lalit",
+        marks: [
+            { subject: "Math", score: 78 },
+            { subject: "English", score: 82 },
+            { subject: "Science", score: 74 },
+            { subject: "History", score: 69 },
+            { subject: "Computer", score: 88 }
+        ],
+        attendance: 82
+    },
+    {
+        name: "Rahul",
+        marks: [
+            { subject: "Math", score: 90 },
+            { subject: "English", score: 85 },
+            { subject: "Science", score: 80 },
+            { subject: "History", score: 76 },
+            { subject: "Computer", score: 92 }
+        ],
+        attendance: 91
+    },
+    {
+        name: "Aman",
+        marks: [
+            { subject: "Math", score: 50 },
+            { subject: "English", score: 55 },
+            { subject: "Science", score: 60 },
+            { subject: "History", score: 58 },
+            { subject: "Computer", score: 62 }
+        ],
+        attendance: 70
+    }
+];
