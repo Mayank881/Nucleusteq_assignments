c7ff9f048b189566362bffdb3164bc0ef74b9d20
Mayank <mayankkukraja.2006@gmail.com>
2026-04-19 01:48:05 +0530

    added CustomException for showing specific msg

---
+++ MayankKukraja_java_training/java/session2/springboot_project/src/main/java/com/mayank/session2/exception/CustomException.java
@@ -0,0 +1,10 @@
+package com.mayank.session2.exception;
+
+// custom exception class
+
+public class CustomException extends RuntimeException {
+
+    public CustomException(String message) {
+        super(message);
+    }
+}
