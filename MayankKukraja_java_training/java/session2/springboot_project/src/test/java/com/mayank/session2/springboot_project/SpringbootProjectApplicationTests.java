e87339e4e6217c1cc22cdddb5840fefcebcf172b
Mayank <mayankkukraja.2006@gmail.com>
2026-04-18 23:55:28 +0530

    created updated spring boot project for session2

---
+++ MayankKukraja_java_training/java/session2/springboot_project/src/test/java/com/mayank/session2/springboot_project/SpringbootProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mayank.session2.springboot_project;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
