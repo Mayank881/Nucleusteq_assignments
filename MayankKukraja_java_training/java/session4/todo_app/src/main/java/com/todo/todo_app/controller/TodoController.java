ad50ff52b1a8d60d3578ad3916e7819ce85f4a61
Mayank <mayankkukraja.2006@gmail.com>
2026-04-20 23:16:31 +0530

    added update and delete todo apis

--- MayankKukraja_java_training/java/session4/todo_app/src/main/java/com/todo/todo_app/controller/TodoController.java
+++ MayankKukraja_java_training/java/session4/todo_app/src/main/java/com/todo/todo_app/controller/TodoController.java
@@ -18,18 +18,33 @@ public class TodoController {
     public TodoController(TodoService service) {
         this.service = service;
     }
-   
+
     // it handles the post request to create a new todo
-    // we use @valid ,,@requestbody to validate the incoming data and map it to our dto
+    // we use @valid ,,@requestbody to validate the incoming data and map it to our
+    // dto
     @PostMapping
     public Todo createTodo(@Valid @RequestBody TodoDTO dto) {
         return service.createTodo(dto);
     }
-   
 
-    //handles get request to fetch all todos
+    // handles get request to fetch all todos
     @GetMapping
     public List<Todo> getAllTodos() {
-    return service.getAllTodos();
-}
+        return service.getAllTodos();
+    }
+
+    // putmapping will handle the update request to update existing todo
+    // path variable get the id and @requestbody get the updated data from user
+    @PutMapping("/{id}")
+    public Todo updateTodo(@PathVariable Long id, @RequestBody TodoDTO dto) {
+        return service.updateTodo(id, dto);
+    }
+
+    // @dkeletemapping will handle the delete request to delete existing todo using
+    // id
+    @DeleteMapping("/{id}")
+    public String deleteTodo(@PathVariable Long id) {
+        service.deleteTodo(id);
+        return "Todo deleted successfully";
+    }
 }
