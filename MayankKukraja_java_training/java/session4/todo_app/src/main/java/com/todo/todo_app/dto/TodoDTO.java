0e3113c0509c56f4b50e6737ecd53937fdfc7c22
Mayank <mayankkukraja.2006@gmail.com>
2026-04-20 22:19:48 +0530

    completed todo dto with validation and proper getters setters

---
+++ MayankKukraja_java_training/java/session4/todo_app/src/main/java/com/todo/todo_app/dto/TodoDTO.java
@@ -0,0 +1,48 @@
+package com.todo.todo_app.dto;
+
+import com.todo.todo_app.entity.Status;
+import jakarta.validation.constraints.NotNull; // validation annotation to ensure title is not null
+import jakarta.validation.constraints.Size;    //to ensure title has a minimum length of 3 characters
+
+// TodoDTO transfers data between client and server
+public class TodoDTO {
+
+    @NotNull
+    @Size(min = 3)
+    private String title;
+
+    private String description;
+
+    private Status status;
+
+    // Constructor
+    public TodoDTO() {}
+
+    // Getters and Setters
+
+    public String getTitle() {
+        return title;
+    }
+    // setter for title
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getDescription() {
+
+        return description;
+    }
+
+    public void setDescription(String description) { 
+
+        this.description = description;
+    }
+
+    public Status getStatus() {     
+
+        return status;
+    }
+   //  setter for status
+    public void setStatus(Status  status) {           
+    }
+}
